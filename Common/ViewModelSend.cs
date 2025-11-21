a7ce36b66b9ba2abc4afbbb885010f8ff18ab321
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 22:02:18 +0500

    Создан класс User Капустин

--- Common/ViewModelSend.cs
+++ Common/ViewModelSend.cs
@@ -2,5 +2,16 @@
 
 public class ViewModelSend
 {
+    /// <summary> Сообщение отправляемое сервером </summary>
+    public string Message { get; set; }
 
+    /// <summary> Код пользователя </summary>
+    public int Id { get; set; }
+
+    /// <summary> Конструктор для заполнения класса </summary>
+    public ViewModelSend(string message, int Id)
+    {
+        this.Message = message;
+        this.Id = Id;
+    }
 }
---
+++ Server/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Server
+{
+    public class User
+    {
+        /// <summary> Логин пользователя </summary>
+        public string login { get; set; }
+        /// <summary> Пароль пользователя </summary>
+        public string password { get; set; }
+        /// <summary> Директория пользователя </summary>
+        public string src { get; set; }
+        /// <summary> Директория пользователя в которой он находится </summary>
+        public string temp_src { get; set; }
+        /// <summary> Конструктор для заполнения класса </summary>
+        public User(string login, string password, string src)
+        {
+            this.login = login;
+            this.password = password;
+            this.src = src;
+
+            temp_src = src;
+        }
+    }
+}
