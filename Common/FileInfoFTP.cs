27cc207b6911c1b3bfd432da3606c7f184a963f7
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 21:58:22 +0500

    Создан класс FileInfoFTP

---
+++ Common/FileInfoFTP.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class FileInfoFTP
+    {
+        /// <summary> Массив байт </summary>
+        public byte[] Data { get; set; }
+
+        /// <summary> Имя файла </summary>
+        public string Name { get; set; }
+
+        /// <summary> Конструктор для заполнения класса </summary>
+        public FileInfoFTP(byte[] Data, string Name)
+        {
+            this.Data = Data;
+            this.Name = Name;
+        }
+    }
+
+}
