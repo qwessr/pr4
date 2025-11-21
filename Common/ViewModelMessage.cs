3dccae33c8533d4490d12bdd1a49452da445eaab
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 21:56:41 +0500

    Создан класс ViewModelMessage Капустин

---
+++ Common/ViewModelMessage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class ViewModelMessage
+    {
+        /// <summary> Команда </summary>
+        public string Command { get; set; }
+
+        /// <summary> Текст ответа </summary>
+        public string Data { get; set; }
+
+        /// <summary> Конструктор для заполнения данных </summary>
+        public ViewModelMessage(string Command, string Data)
+        {
+            this.Command = Command;
+            this.Data = Data;
+        }
+    }
+}
