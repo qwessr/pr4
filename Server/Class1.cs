f4aafff7a14d2ee7fb304f93c92a55e854a81176
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 21:52:49 +0500

    создан класс ViewModelSend капустин

---
+++ Server/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace Server
+{
+    public class ViewModelSend
+    {
+        /// <summary> Сообщение отправляемое сервером </summary>
+        public string Message { get; set; }
+
+        /// <summary> Код пользователя </summary>
+        public int Id { get; set; }
+
+        /// <summary> Конструктор для заполнения класса </summary>
+        public ViewModelSend(string message, int Id)
+        {
+            this.Message = message;
+            this.Id = Id;
+        }
+    }
+}
