38e5da26eda62f8681511a9856d12b0dad12f9a8
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 22:06:40 +0500

    Прописан метод AutorizationUser Капустин

--- Server/Program.cs
+++ Server/Program.cs
@@ -15,5 +15,17 @@ namespace Server
         public static IPAddress IpAddress;
         /// <summary> Порт сервера </summary>
         public static int Port;
+
+
+        /// <summary> Авторизация пользователя </summary>
+        public static bool AutorizationUser(string login, string password)
+        {
+            // Создаём объект отвечающий за авторизованного пользователя
+            User user = null;
+            // Среди всех пользователей ищем пользователя с удовлетворяющим логином и паролем
+            user = Users.Find(x => x.login == login && x.password == password);
+            // Возвращаем результат что пользователь не равен null
+            return user != null;
+        }
     }
 }
