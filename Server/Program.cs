13c4a417ea8f8135e3474dfcf601b0767d88d26e
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 22:05:29 +0500

    прописаны классы в классе Program на Server Капустин

---
+++ Server/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        /// <summary> Список пользователей </summary>
+        public static List<User> Users = new List<User>();
+        /// <summary> IP-адрес сервера </summary>
+        public static IPAddress IpAddress;
+        /// <summary> Порт сервера </summary>
+        public static int Port;
+    }
+}
