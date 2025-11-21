14a46bf412402267caa7ad38de3542a61366a6bc
Iliya12 <tankist228121234567890@gmail.com>
2025-11-21 22:07:30 +0500

    Прописан класс GetDirectory Капустин

--- Server/Program.cs
+++ Server/Program.cs
@@ -27,5 +27,38 @@ namespace Server
             // Возвращаем результат что пользователь не равен null
             return user != null;
         }
+
+        /// <summary> Получение директорий </summary>
+        public static List<string> GetDirectory(string src)
+        {
+            // Создаём список директорий
+            List<string> FoldersFiles = new List<string>();
+            // Проверяем что директория от которой мы отталкиваемся существует
+            if (Directory.Exists(src))
+            {
+                // Получаем список всех директорий
+                string[] dirs = Directory.GetDirectories(src);
+                // Перебираем через цикл директории
+                foreach (string dir in dirs)
+                {
+                    // Создаём новое имя для директории, заменяя часть пути
+                    string NameDirectory = dir.Replace(src, "");
+                    // Добавляем директорию в массив
+                    FoldersFiles.Add(NameDirectory + "/");
+                }
+                // Получаем список всех файлов находящиеся в директории
+                string[] files = Directory.GetFiles(src);
+                // Перебираем через цикл файлы
+                foreach (string file in files)
+                {
+                    // Создаём новое имя для файла, заменяя часть пути
+                    string NameFile = file.Replace(src, "");
+                    // Добавляем файл в массив
+                    FoldersFiles.Add(NameFile);
+                }
+            }
+
+            return FoldersFiles;
+        }
     }
 }
